86160bcce1217cf13f63434274b2c376f3cada3a
Vasiliy Razdorskiy <vrazdorsky@yandex.ru>
2022-07-14 12:39:56 +0300

    Добавить попап добавления поста

--- scripts/index.js
+++ scripts/index.js
@@ -1,32 +1,87 @@
-let popup = document.querySelector(".popup");
-let profileEditButton = document.querySelector(".profile__edit-button");
-let popupCloseButton = popup.querySelector(".popup__close-button");
-let popupInputName = popup.querySelector(".popup__input_text_name");
-let popupInputMoreInfo = popup.querySelector(".popup__input_text_more-info");
-let profileName = document.querySelector(".profile__name");
-let profileMoreInfo = document.querySelector(".profile__more-info");
-let popupForm = popup.querySelector(".popup__form");
-
-let popupOpen = function() {
-    popup.classList.add("popup_active");
-    popupInputName.value = profileName.textContent;
-    popupInputMoreInfo.value = profileMoreInfo.textContent;
+const selectors = {
+  popupProfileEdit: ".popup_action_edit",
+  profileEditButton: ".profile__edit-button",
+  popupProfileEditCloseButton: ".popup__close-button_place_edit-popup",
+  popupInputName: ".popup__input_text_profile-name",
+  popupInputMoreInfo: ".popup__input_text_profile-more-info",
+  profileName: ".profile__name",
+  profileMoreInfo: ".profile__more-info",
+  popupProfileEditForm: ".popup__form_place_edit-popup",
+  profileAddPostButton: ".profile__add-button",
+  popupAddPost: ".popup_action_add-post",
+  popupAddPostCloseButton: ".popup__close-button_place_add-post-popup",
+  popupAddPostForm: ".popup__form_place_add-post-popup",
 };
 
-let popupClose = function() {
-    popup.classList.remove("popup_active");
-};
-
-let formSubmitHandler = function(evt) {
-    evt.preventDefault();
-    profileName.textContent = popupInputName.value;
-    profileMoreInfo.textContent = popupInputMoreInfo.value;
-    popupClose();
+function popupOpen(popup) {
+  popup.classList.add("popup_active");
 }
 
+function popupClose(popup) {
+  popup.classList.remove("popup_active");
+}
 
+// Попап редактирования профиля
+const popupProfileEdit = document.querySelector(selectors.popupProfileEdit);
+const popupInputName = popupProfileEdit.querySelector(selectors.popupInputName);
+const profileName = document.querySelector(selectors.profileName);
+const popupInputMoreInfo = popupProfileEdit.querySelector(
+  selectors.popupInputMoreInfo
+);
+const profileMoreInfo = document.querySelector(selectors.profileMoreInfo);
+const profileEditButton = document.querySelector(selectors.profileEditButton);
+const popupProfileEditCloseButton = popupProfileEdit.querySelector(
+  selectors.popupProfileEditCloseButton
+);
+const popupProfileEditForm = popupProfileEdit.querySelector(
+  selectors.popupProfileEditForm
+);
 
+const popupProfileEditOpen = function () {
+  popupOpen(popupProfileEdit);
+  popupInputName.value = profileName.textContent;
+  popupInputMoreInfo.value = profileMoreInfo.textContent;
+};
 
-profileEditButton.addEventListener("click", popupOpen);
-popupCloseButton.addEventListener("click", popupClose);
-popupForm.addEventListener("submit", formSubmitHandler);
+const popupProfileEditClose = function () {
+  popupClose(popupProfileEdit);
+};
+
+const formSubmitHandlerProfileEdit = function (evt) {
+  evt.preventDefault();
+  profileName.textContent = popupInputName.value;
+  profileMoreInfo.textContent = popupInputMoreInfo.value;
+  popupClose(popupProfileEdit);
+};
+
+// Попап добавления постов
+const popupAddPost = document.querySelector(selectors.popupAddPost);
+const profileAddPostButton = document.querySelector(
+  selectors.profileAddPostButton
+);
+const popupAddPostCloseButton = popupAddPost.querySelector(
+  selectors.popupAddPostCloseButton
+);
+
+const popupAddPostOpen = function () {
+  popupOpen(popupAddPost);
+};
+
+const popupAddPostClose = function () {
+  popupClose(popupAddPost);
+};
+
+function addEventListeners() {
+  //Открытие попапов
+  profileEditButton.addEventListener("click", popupProfileEditOpen);
+  profileAddPostButton.addEventListener("click", popupAddPostOpen);
+
+  //Закрытие попапов
+  popupProfileEditCloseButton.addEventListener("click", popupProfileEditClose);
+  popupAddPostCloseButton.addEventListener("click", popupAddPostClose);
+
+  //Обработка введённой информации в попапах
+  popupProfileEditForm.addEventListener("submit", formSubmitHandlerProfileEdit);
+}
+
+addEventListeners();
